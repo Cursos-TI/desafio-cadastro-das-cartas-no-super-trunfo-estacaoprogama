753462b8bdf0be916b2a04e0a8496da1ba2a745e
estacaoprogama <luisccastro56@gmail.com>
2025-03-12 00:25:51 +0000

    criando variaveis

--- Super_trunfo.c
+++ Super_trunfo.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
 int main() {
+    char estado, carta, cidade; // Declara variaveis do tipo char
+    int população, pontosturisticos; // Declara variaveis do tipo int
+    float área, PIB; // Declara variaveis do tipo float
 
     
 
 
+
+
+    printf("Cadastro das Cartas");
+
+
+
+    printf("Carta 1:");
+    printf("Digite uma letra de "A" a "H" referente ao Estado: \n");
+    scanf()
+
+
+    
 }
