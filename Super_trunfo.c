962a7d544eaaa9f122a094ae31f2c28697c5ebc8
estacaoprogama <luisccastro56@gmail.com>
2025-03-12 01:00:16 +0000

    Corrigi digitação no código

--- Super_trunfo.c
+++ Super_trunfo.c
@@ -2,24 +2,24 @@
 
 int main() {
     char estado, carta[3], cidade[20]; // Declara variaveis do tipo char
-    int população, pontosturisticos; // Declara variaveis do tipo int
+    int populacao, pontosturisticos; // Declara variaveis do tipo int
     float area, pib; // Declara variaveis do tipo float
 
     printf("Cadastro das Cartas\n");
 
     printf("Carta 1: \n");
 
-    printf("Digite uma letra de "A" a "H" referente ao Estado: \n");
+    printf("Digite uma letra de A a H referente ao Estado: \n");
     scanf("%c", &estado);
 
-    printf("Digite o código da carta: \n ");
+    printf("Digite o código da carta: \n");
     scanf("%s", &carta);
 
     printf("Digite a Cidade:\n");
     scanf("%s", &cidade);
 
     printf("Digite a população: \n");
-    scanf("%f", &população);
+    scanf("%f", &populacao);
 
     printf("Digite a área em km²: \n");
     scanf("%f", &area);
